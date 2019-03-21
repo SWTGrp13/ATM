c83aeec910957ac55a89e98cb293c2813ceabb51
mikkel-steen <=>
2019-03-21 12:14:09 +0100

    fixed filestruct for Jenkins

---
+++ TransponderReceiver.Test/UnitTestClasses/CalculationsUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TransponderReceiverLib.Calculations;
+
+
+
+namespace TransponderReceiver.Test.UnitTestClasses
+{
+    [TestFixture]
+    class CalculationsUnitTest
+    {
+        //private Calculate UUT;
+
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //    UUT = new Calculate();
+        //}
+
+
+
+
+        //[TestCase()]
+        //public void FindDegree_TRUE(int x, int y, int degree)
+        //{
+        //    Assert.That(UUT.FindDegree(x, y), Is.EqualTo(degree).Within(0.01));
+        //}
+
+        //[TestCase()]
+        //public void FindDegree_FALSE(int x, int y, int degree)
+        //{
+        //    Assert.That(UUT.FindDegree(x, y), Is.Not.EqualTo(degree).Within(0.01));
+        //}
+
+        //[TestCase()]
+        //public void FindVelocity_TRUE(int x, int xOld, int y, int yOld, int Velocity)
+        //{
+        //    Assert.That(UUT.FindVelocity(x, xOld, y, yOld), Is.EqualTo(Velocity));
+        //}
+
+    }
+}
---
+++ TransponderReceiver/Tracks/Observer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransponderReceiverLib.Tracks
+{
+    public interface IObserver
+    {
+        void Update(string data);
+        string Identify();
+    } 
+
+ 
+}
