feb1348559458745f25f730106afaf572a39068e
Mie Nielsen <au581183@uni.au.dk>
2019-03-24 16:20:41 +0100

    Commit to pull

--- TransponderReceiver.Test/UnitTestClasses/CalculationsUnitTest.cs
+++ TransponderReceiver.Test/UnitTestClasses/CalculationsUnitTest.cs
@@ -16,23 +16,27 @@ namespace TransponderReceiver.Test.UnitTestClasses
     [TestFixture]
     public class CalculationsUnitTest
     {
+
+     // Test of isInValidSpace
+
         private readonly string Time = DateTime.Now.ToString("yyyyMMddHHmmssfff");
 
         [TestCase("ATR423;39045;12932;14000;")]
         [TestCase("BCD123;10005;8890;12000;")]
-        [TestCase("XYZ987;25059;75654;4000;")]
+        [TestCase("XYZ987;70000;9000;19999;")]
+        [TestCase("MIW248;2549;75654;501;")]
         public void TestisInValidSpace_True(string x)
         {
             Assert.That(Calculate.isInValidSpace(Factory.GetPlane(x + Time)), Is.True);
         }
-
 
         [TestCase("ATR423;90045;12932;14000;")] // XPos Invalid
         [TestCase("ATR423;-55;12932;14000;")] // XPos Invalid
         [TestCase("BCD123;10005;90890;12000;")] // YPos Invalid
         [TestCase("BCD123;10005;-890;12000;")] // YPos Invalid
         [TestCase("XYZ987;25059;75654;400;")] // Altitude Invalid
-        [TestCase("KAT130;90045;12932;21000;")] // Altitude Invalid
+        [TestCase("KAT130;9005;12932;21000;")] // Altitude Invalid
+        [TestCase("KAT130;90045;90890;21000;")] // All Invalid
         public void TestisInvalidSpace_PosFalse(string x)
         {
             Assert.That(Calculate.isInValidSpace(Factory.GetPlane(x + Time)), Is.False);
@@ -45,6 +49,8 @@ namespace TransponderReceiver.Test.UnitTestClasses
         {
             Assert.That(Calculate.isInValidSpace(Factory.GetPlane(x)), Is.False);
         }
+
+    //Test of FindDregee
 
         [TestCase("KAT130;0;0;21000;20161006213456789", "KAT130;0;0;21000;20161006213456789", 0)]
         [TestCase("KAT130;6436;33560;21000;20161006213456789", "KAT130;6538;33540;21000;20161006213456789", 349)]
